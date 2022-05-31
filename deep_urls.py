208435b85f6c3df00f811cc2f819248ed5d3e32a
Chloe Stout <chadstout@scratchpeg.io>
2022-05-30 21:50:11 -0500

    need to fix error code with RequestException and one error exceeding 64 url characters.  Then need to print list of actual working websites

--- deep_urls.py
+++ deep_urls.py
@@ -1,11 +1,5 @@
-# from selenium import webdriver
-# from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-#
-# binary = FirefoxBinary("/usr/bin/tor-browser")
-# driver = webdriver.Firefox(service=binary)
-# url = "https://duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion/"
-# driver.get(url)
+import mod_deep_urls as mdu
 
-import mod_deep_urls
 
-mod_deep_urls.run_urls()
+mdu.run_urls()
+# mdu.http_or_https()
--- mod_deep_urls.py
+++ mod_deep_urls.py
@@ -1,6 +1,14 @@
 import random
 import string
-import urllib
+import requests
+
+
+def http_or_https():
+    num = random.randint(0, 1)
+    if num == 0:
+        return 's'
+    else:
+        return ''
 
 
 def ran_num():
@@ -28,11 +36,54 @@ def urls_list():
     return "".join(map(str, *list_of_urls))
 
 
+def test_url():
+    # return "http://s4k4ceiapwwgcm3mkb6e4diqecpo7kvdnfr5gg7sph7jjppqkvwwqtyd.onion/"
+    return "http://A1017QWV16FSDYXrymxIsz81LZhBf2R18711M50n13w619U914N15412a3Ej.onion"
+
+
+def tor_url_response():
+    try:
+        session = requests.session()
+        session.proxies = {'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}
+        r = session.get(f'http{http_or_https()}://{urls_list()}.onion/')
+        # r = session.get(f'{urls_list()}')
+        return r.status_code
+    except requests.exceptions.RequestException:
+        return None
+
+
+def active_list():
+    print(tor_url_response())
+    if tor_url_response() == 200:
+        list_of_urls = []
+        listing = urls_list()
+        list_of_urls.append(listing)
+        return print("".join(map(str, *list_of_urls)))
+
+
+def tor_content(site_content):
+    try:
+        session = requests.session()
+        session.proxies = {'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}
+        r = session.get(site_content)
+        print(r.content)
+    except Exception as err:
+        print("No Content")
+
+
+def tor_headers(site_headers):
+    try:
+        session = requests.session()
+        session.proxies = {'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}
+        r = session.get(site_headers)
+        print(r.headers)
+    except Exception as err:
+        print("No Headers")
+
+
 def run_urls():
     urls = input('Enter amount of Urls: ')
     for i in range(int(urls)):
-        ran_num()
-        ran_let()
-        shuffle_list()
-        print("http://", urls_list(), ".onion", sep='')
+        active_list()
+        # print(f"https://{urls_list()}.onion", sep='')
 
